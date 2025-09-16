28bf39e8c11891ea2d0342e77ef139111db66adb
SavraSSS <spa64463@gmail.com>
2025-09-17 02:01:07 +0300

    added diffirent phrases

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import random
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
 from aiogram.fsm.state import StatesGroup, State
@@ -8,6 +9,11 @@ from aiogram.fsm.context import FSMContext
 from db import DB
 
 API_TOKEN = ""
+
+err_phr = ['Упс... такой команды нет', 'Такой команды я пока не знаю, но обязательно узнаю!', 'Я не знаю эту команду']
+
+rand_phr = ['Вот тебе случайный фильм на сегодня:', 'Посмотри вот это:', 'Рекомендую посмотреть', 'Рандом есть рандом, придется смотреть']
+rand_mas = ['Остров проклятых', 'Престиж', 'Интерстеллар', 'Титаник', 'Хатико', 'Смешарики', 'Начало', 'Довод', 'Миссия невыполнима', 'Назад в будущее']
 
 logging.basicConfig(level=logging.INFO)
 
@@ -26,24 +32,32 @@ async def start_handler(message: types.Message, state: FSMContext):
     #    user_id=message.from_user.id,
     #    username=message.from_user.username)
     await state.set_state(FavMovie.waiting_for_title)
-    await message.answer('Привет! Напиши любимый фильм')
+    await message.answer('Привет! Какой твой любимый фильм?')
 
 @dp.message(Command(commands = ['favorite_film', 'Favorite_film']))
 async def fav_handler(message: types.Message, state: FSMContext):
     await state.set_state(FavMovie.waiting_for_title)
     await message.answer("Какой твой любимый фильм?")
 
+@dp.message(Command(commands = ['random_film', 'Random_film']))
+async def rand_handler(message: types.Message, state: FSMContext):
+    await message.answer(random.choice(rand_phr) + ' ' + random.choice(rand_mas))
 
 @dp.message(Command(commands=['info','Info']))
 async def info_handler(message: types.Message, state: FSMContext):
-    await message.answer("""Основная цель существования бота - помочь с выбором фильмов и сериалов, подсказать на основе предпочтений пользователя\nРазработчик - SavraSSS\n2025
-                         """)
+    await message.answer("Основная цель существования бота - помочь с выбором фильмов и сериалов, подсказать на основе ваших предпочтений\nРазработчик - SavraSSS\n2025")
 
+@dp.message(F.text.startswith("/"))
+async def catch_any_command(message: types.Message):
+    await message.answer(random.choice(err_phr))
+    await message.answer(f"Попробуйте ввести другую команду вместо {message.text}")
 
 @dp.message(FavMovie.waiting_for_title, F.text)
 async def process_fav(message: types.Message, state: FSMContext):
     title = message.text.strip()
-    if title[0] != '/':
+    if title == 'Букины' or title == 'букины':
+        await message.answer(f"ООО, {title} 10/10")
+    elif title[0] != '/':
         await message.answer(f"Запомнил твой любимый фильм: {title}")
     else:
         await message.answer(f"К сожалению это не фильм")
@@ -52,7 +66,7 @@ async def process_fav(message: types.Message, state: FSMContext):
 
 @dp.message()
 async def update_handler(message: types.Message):
-    await message.answer('Снова здарова')
+    await message.answer('Здарова')
 
 
 async def main():
